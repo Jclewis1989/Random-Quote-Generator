e4e6c85cced669330fe67283d4d8ce7a4dea3137
James Crane Lewis III <james.crane.lewis.iii@gmail.com>
2017-04-14 07:46:15 -0700

    Ajdusting my if / else statement
    Correcting randomQuote.year
    
    Replacing randomQuote.citation with randomQuote.year

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
+//To simplify and understand how functions return what they should, I made sure to follow all instructions as best as possible by   commenting them at their respective parts in the code below.
 
-    //To simplify and understand how functions return what they should, I made sure to follow all instructions as best as possible by   commenting them at their respective parts in the code below.
-    
-    //event listener to respond to "Show another quote" button clicks
-    //when user clicks anywhere on the button, the "printQuote" function is called
-    document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-  
-    // Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes. The quotes array should be accessible in the global scope.
-    
-    // A quote property which contains a string: the text of the quote that will be displayed on the page.
+//event listener to respond to "Show another quote" button clicks
+//when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
-    // A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
+// Create an array of JavaScript objects to hold the data for your quotes. Name the array quotes. The quotes array should be accessible in the global scope.
 
-    // An optional citation property which contains a string identifying where the quote comes from, like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
-   
-    // An optional year property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.
-    
+// A quote property which contains a string: the text of the quote that will be displayed on the page.
+
+// A source property which contains a string identifying the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb.”
+
+// An optional citation property which contains a string identifying where the quote comes from, like a speech or publication. For example, "Famous Anonymous Jokes." If there is no known publication, do not include this property on the object.
+
+// An optional year property which contains a number identifying the date of the quote. For example, 1997. If there is no known date, then do not include this property on the object.
+
 var quotes = [
 
     // Sport Quotes
@@ -66,41 +65,41 @@ var quotes = [
   },
 ];
 
-  // selects a random quote object from the quotes array
-  // returns the randomly selected quote object
+// selects a random quote object from the quotes array
+// returns the randomly selected quote object
 function getRandomQuote() {
-  var randomQuote = Math.floor(Math.random() * quotes.length);
-  return quotes[randomQuote];
+    var randomQuote = Math.floor(Math.random() * quotes.length);
+    return quotes[randomQuote];
 }
 
 function printQuote() {
-  
-  // Calling additional functions for 'exceed' expectations
-  changeColor();
-  
-  // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
-  var randomQuote = getRandomQuote();
-  
-  // printQuote constructs a string containing the different properties of the quote object using the following HTML template:
-  var quotation = '<p class="quote">' + randomQuote.quotation + '</p>';
-  quotation += '<p class="source">' + randomQuote.source;
-    
-  // printQuote doesn't add a for a missing citation or a if the year property is missing
-    if(randomQuote.citation) {
-  quotation += '<p class="citation">' + randomQuote.citation;        
+
+    // Calling additional functions for 'exceed' expectations
+    changeColor();
+
+    // printQuote calls the getRandomQuote function and stores the returned quote object in a variable
+    var randomQuote = getRandomQuote();
+
+    // printQuote constructs a string containing the different properties of the quote object using the following HTML template:
+    var quotation = '<p class="quote">' + randomQuote.quotation + '</p>';
+    quotation += '<p class="source">' + randomQuote.source;
+
+    // printQuote doesn't add a for a missing citation or a if the year property is missing
+    if (randomQuote.citation) {
+        quotation += '<p class="citation">' + randomQuote.citation;
     } else {
         randomQuote.citation = null;
     }
-    
-  // printQuote doesn't add a for a missing citation or a if the year property is missing
-  if(randomQuote.year) {
-    quotation += '<span class="citation">' + randomQuote.citation + '</span>';
-  } else {
-    randomQuote.year = null;
-  }
-  
-  // printQuote displays the final HTML string to the page. You can use this JS snippet to accomplish that
-  return document.getElementById('quote-box').innerHTML = quotation;
+
+    // printQuote doesn't add a for a missing citation or a if the year property is missing
+    if (randomQuote.year) {
+        quotation += '<span class="year">' + randomQuote.year + '</span>';
+    } else {
+        randomQuote.year = null;
+    }
+
+    // printQuote displays the final HTML string to the page. You can use this JS snippet to accomplish that
+    return document.getElementById('quote-box').innerHTML = quotation;
 }
 
 // Add comments to your code.
@@ -118,13 +117,13 @@ function printQuote() {
 
 // When the quote changes, randomly change the background color of the page.
 
-function changeColor () {
-  var colors = ['orange', 'green', 'red', 'yellow', 'purple', 'blue', 'pink'];
-  var randomColors = Math.floor(Math.random() * colors.length); // Emphasis on randomly! Almost forgot that part
-  var color = document.getElementById('body');
-  for (var key in colors[randomColors]) {
-    color.style.backgroundColor = colors[randomColors];
-  }
+function changeColor() {
+    var colors = ['orange', 'green', 'red', 'yellow', 'purple', 'blue', 'pink'];
+    var randomColors = Math.floor(Math.random() * colors.length); // Emphasis on randomly! Almost forgot that part
+    var color = document.getElementById('body');
+    for (var key in colors[randomColors]) {
+        color.style.backgroundColor = colors[randomColors];
+    }
 }
 
 // _________________________________________________REQUIREMENT 3__________________________________________________
@@ -137,9 +136,9 @@ function changeColor () {
 
 // Refresh the quote after a set amount of time. For example, every 30 seconds, make a new quote appear. (You can use the setInterval() or setTimeout() method to do this -- see the links in the “Additional Resources” section).
 
-setInterval(function(){
-  printQuote();
-},30000);
+setInterval(function () {
+    printQuote();
+}, 30000);
 
 // Seconds in increments of 1000. I did not know that. 30 seconds as requested.
 // The problem I am now experiencing is the color changes too. Not just the quote.
